0f23b05a2909d3e1a45455f0565730443675b5f0
srivaniganji <srivaniganji2805@gmail.com>
2024-05-16 12:31:14 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import logo from "./logo.svg";
-import Gamae from "./Game";
+import "./App.css";
+
 function App() {
   return (
     <div className="App">
-      <Game />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
